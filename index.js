38b2a3e0497577bf5f67e4719822a6e7ef2a33c3
Leandro França dos Santos <leandrofrancadossantos@gmail.com>
2022-07-05 17:30:53 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+
+function returnEvenValues(array){
+    let evenNums = [];
+    for (let i = 0; i < array.length; i++){
+        if(array[i] % 2 === 0){
+            evenNums.push(array[i]);
+        }else {
+            console.log(`${array[i]} não é par`);
+        }
+    }
+    console.log(evenNums);
+}
+
+let array = [1,2,3,4,5,8];
+returnEvenValues(array);
+
+console.log('eu ja falei que estou aqui')
